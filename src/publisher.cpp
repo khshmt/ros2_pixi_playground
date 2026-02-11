399742e0c65c0c3f29c709ab1adc6aeb4e94371b
khshmt <khshmt2@gmail.com>
2026-02-11 14:42:30 +0200

    add publisher and subscriber tasks in pixi.toml

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     rclcpp::init(argc, argv);
     auto node = std::make_shared<rclcpp::Node>("publisher_node");
     auto pub_ = node->create_publisher<std_msgs::msg::String>("my_topic", 10);
-    rclcpp::Rate loop_rate(10); // 1 Hz
+    rclcpp::Rate loop_rate(10); // 10 Hz
     while (rclcpp::ok())
     {
         std_msgs::msg::String msg;
