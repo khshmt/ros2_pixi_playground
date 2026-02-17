cdf2236bf4b17eb624e51351c3c49e1adb6a10e0
khshmt <khshmt2@gmail.com>
2026-02-17 14:31:54 +0200

    format DDS publisher and subscriber and add qos for the publisher

--- src/CycloneDDS/framePublisher.cpp
+++ src/CycloneDDS/framePublisher.cpp
@@ -1,85 +1,88 @@
-#include "dds/dds.h"
-#include "Image.h"
 #include <opencv2/opencv.hpp>
+#include "Image.h"
+#include "dds/dds.h"
 
 int main(int argc, char* argv[]) {
-    dds_entity_t participant;
-    dds_entity_t topic;
-    dds_entity_t writer;
-    dds_return_t rc;
-    camera_Image msg;
-    uint32_t status = 0;
-    (void)argc;
-    (void)argv;
+  dds_entity_t participant;
+  dds_entity_t topic;
+  dds_entity_t writer;
+  dds_return_t rc;
+  camera_Image msg;
+  dds_qos_t* qos;
+  uint32_t status = 0;
+  (void)argc;
+  (void)argv;
 
-    participant = dds_create_participant(DDS_DOMAIN_DEFAULT, NULL, NULL);
-    if (participant < 0)
-        DDS_FATAL("dds_create_participant: %s\n", dds_strretcode(-participant));
+  // Create a Participant.
+  participant = dds_create_participant(DDS_DOMAIN_DEFAULT, NULL, NULL);
+  if (participant < 0)
+    DDS_FATAL("dds_create_participant: %s\n", dds_strretcode(-participant));
 
-    topic = dds_create_topic(
-        participant,
-        &camera_Image_desc,
-        "camera/Image",
-        NULL,
-        NULL);
-    if (topic < 0)
-        DDS_FATAL("dds_create_topic: %s\n", dds_strretcode(-topic));
+  // Create a Topic.
+  topic = dds_create_topic(participant, &camera_Image_desc, "camera/Image",
+                           NULL, NULL);
+  if (topic < 0)
+    DDS_FATAL("dds_create_topic: %s\n", dds_strretcode(-topic));
 
-    writer = dds_create_writer(participant, topic, NULL, NULL);
-    if (writer < 0)
-        DDS_FATAL("dds_create_writer: %s\n", dds_strretcode(-writer));
+  // create a reliable Writer
+  qos = dds_create_qos();
+  dds_qset_reliability(qos, DDS_RELIABILITY_RELIABLE, DDS_SECS(1));
+  writer = dds_create_writer(participant, topic, qos, NULL);
+  if (writer < 0)
+    DDS_FATAL("dds_create_writer: %s\n", dds_strretcode(-writer));
+  dds_delete_qos(qos);
 
-    rc = dds_set_status_mask(writer, DDS_PUBLICATION_MATCHED_STATUS);
+  // Wait for a subscriber to match before writing data
+  rc = dds_set_status_mask(writer, DDS_PUBLICATION_MATCHED_STATUS);
+  if (rc != DDS_RETCODE_OK)
+    DDS_FATAL("dds_set_status_mask: %s\n", dds_strretcode(-rc));
+  while (!(status & DDS_PUBLICATION_MATCHED_STATUS)) {
+    rc = dds_get_status_changes(writer, &status);
     if (rc != DDS_RETCODE_OK)
-        DDS_FATAL("dds_set_status_mask: %s\n", dds_strretcode(-rc));
+      DDS_FATAL("dds_get_status_changes: %s\n", dds_strretcode(-rc));
+    /* Polling sleep. */
+    dds_sleepfor(DDS_MSECS(20));
+  }
 
-    while(!(status & DDS_PUBLICATION_MATCHED_STATUS))
-    {
-        rc = dds_get_status_changes(writer, &status);
-        if (rc != DDS_RETCODE_OK)
-            DDS_FATAL("dds_get_status_changes: %s\n", dds_strretcode(-rc));
-
-        /* Polling sleep. */
-        dds_sleepfor(DDS_MSECS(20));
-    }
 #ifdef _WIN32
-    cv::VideoCapture cap(0, cv::CAP_DSHOW);
+  cv::VideoCapture cap(0, cv::CAP_DSHOW);
 #elif __linux__
-    cv::VideoCapture cap(0);
+  cv::VideoCapture cap(0);
 #endif
-    //Force MJPG
-    cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
-    //Set resolution
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
-    //Set FPS
-    cap.set(cv::CAP_PROP_FPS, 30);
-    //Reduce buffering
-    cap.set(cv::CAP_PROP_BUFFERSIZE, 1);
+  //Force MJPG
+  cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+  //Set resolution
+  cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+  cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+  //Set FPS
+  cap.set(cv::CAP_PROP_FPS, 30);
+  //Reduce buffering
+  cap.set(cv::CAP_PROP_BUFFERSIZE, 1);
 
-    size_t size{};
-    { // Grab one frame to get the size of the buffer needed for the DDS message
-      cv::Mat frame;
-      if (cap.read(frame)) {
-        size = frame.total() * frame.elemSize();
-        msg.data._length = size;
-        msg.data._maximum = size;
-        msg.data._release = false;
-        msg.data._buffer = (uint8_t*)malloc(size);
-      }
+  size_t size{};
+  {  // Grab one frame to get the size of the buffer needed for the DDS message
+    cv::Mat frame;
+    if (cap.read(frame)) {
+      size = frame.total() * frame.elemSize();
+      msg.data._length = size;
+      msg.data._maximum = size;
+      msg.data._release = false;
+      msg.data._buffer = (uint8_t*)malloc(size);
     }
+  }
 
-    while (true) {
-        cv::Mat frame;
-        if (!cap.read(frame))
-            continue;
+  while (true) {
+    cv::Mat frame;
+    if (!cap.read(frame))
+      continue;
 
-        memcpy(msg.data._buffer, frame.data, size);
-        msg.width = frame.cols;
-        msg.height = frame.rows;
-        msg.timestamp = std::chrono::steady_clock::now().time_since_epoch().count();
+    memcpy(msg.data._buffer, frame.data, size);
+    msg.width = frame.cols;
+    msg.height = frame.rows;
+    msg.timestamp = std::chrono::steady_clock::now().time_since_epoch().count();
 
-        dds_write(writer, &msg);
-    }
-    dds_delete(participant);
+    dds_write(writer, &msg);
+  }
+  dds_free(msg.data._buffer);
+  dds_delete(participant);
 }
--- src/CycloneDDS/frameSubscriber.cpp
+++ src/CycloneDDS/frameSubscriber.cpp
@@ -1,9 +1,9 @@
-#include "dds/dds.h"
-#include "Image.h"
-#include <thread>
 #include <chrono>
 #include <opencv2/opencv.hpp>
 #include <rerun.hpp>
+#include <thread>
+#include "Image.h"
+#include "dds/dds.h"
 
 #define MAX_SAMPLES 1
 
@@ -11,38 +11,39 @@ int main(int argc, char* argv[]) {
   dds_entity_t participant;
   dds_entity_t topic;
   dds_entity_t reader;
-  camera_Image *msg;
-  void *samples[MAX_SAMPLES];
+  camera_Image* msg;
+  void* samples[MAX_SAMPLES];
   dds_sample_info_t infos[MAX_SAMPLES];
   dds_return_t rc;
-  dds_qos_t *qos;
+  dds_qos_t* qos;
   (void)argc;
   (void)argv;
+
+  // rerun setup
   const rerun::RecordingStream rec("DDS_camera_stream");
   rec.spawn(rerun::SpawnOptions{.memory_limit = "1GB"}).exit_on_failure();
-  if(!rec.is_enabled()) {
-      DDS_FATAL("Failed to start rerun viewer!!\n");
+  if (!rec.is_enabled()) {
+    DDS_FATAL("Failed to start rerun viewer!!\n");
   }
-  /* Create a Participant. */
-  participant = dds_create_participant (DDS_DOMAIN_DEFAULT, NULL, NULL);
+
+  // Create a Participant.
+  participant = dds_create_participant(DDS_DOMAIN_DEFAULT, NULL, NULL);
   if (participant < 0)
     DDS_FATAL("dds_create_participant: %s\n", dds_strretcode(-participant));
 
-  topic = dds_create_topic (
-  participant, &camera_Image_desc, "camera/Image", NULL, NULL);
+  // Create a Topic.
+  topic = dds_create_topic(participant, &camera_Image_desc, "camera/Image",
+                           NULL, NULL);
   if (topic < 0)
     DDS_FATAL("dds_create_topic: %s\n", dds_strretcode(-topic));
 
-  /* Create a reliable Reader. */
+  // Create a reliable Reader.
   qos = dds_create_qos();
-  dds_qset_reliability(qos, DDS_RELIABILITY_RELIABLE, DDS_SECS(10));
+  dds_qset_reliability(qos, DDS_RELIABILITY_RELIABLE, DDS_SECS(1));
   reader = dds_create_reader(participant, topic, qos, NULL);
   if (reader < 0)
     DDS_FATAL("dds_create_reader: %s\n", dds_strretcode(-reader));
   dds_delete_qos(qos);
-
-  printf ("\n=== [Subscriber] Waiting for a sample ...\n");
-  fflush (stdout);
 
   samples[0] = camera_Image__alloc();
   while (true) {
@@ -52,16 +53,18 @@ int main(int argc, char* argv[]) {
     if (rc > 0 && infos[0].valid_data) {
       msg = static_cast<camera_Image*>(samples[0]);
       rec.log("camera/image", rerun::Image(msg->data._buffer,
-                                          {static_cast<uint32_t>(msg->width),
-                                           static_cast<uint32_t>(msg->height)},
-                                          rerun::datatypes::ColorModel::BGR));
+                                           {static_cast<uint32_t>(msg->width),
+                                            static_cast<uint32_t>(msg->height)},
+                                           rerun::datatypes::ColorModel::BGR));
       std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
   }
+
   camera_Image_free(samples[0], DDS_FREE_ALL);
-  /* Deleting the participant will delete all its children recursively as well. */
-  rc = dds_delete (participant);
+  // Deleting the participant will delete all its children recursively as well.
+  rc = dds_delete(participant);
   if (rc != DDS_RETCODE_OK)
     DDS_FATAL("dds_delete: %s\n", dds_strretcode(-rc));
+
   return 0;
 }
